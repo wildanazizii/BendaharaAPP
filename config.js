9f2e77286fea90c4e88bf52afc476c4a048dafc4
wildanazizi <aziziwildan03@gmail.com>
2025-01-08 09:35:05 +0700

    14

--- config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    CLIENT_ID: '540904698452-dtu5o8ph525i3rh4vq37ce3o0pms0e0j.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyCbIM4678qIdPy_--FGno5crIl8z14qH8c',
-    SPREADSHEET_ID: '1Tj5BFK0DOLO8-RxTUju9mL0UIs1ejuJRPDyp2gTTXC40',
-    SHEET_NAME: 'Bendahara-AQ',
-    DISCOVERY_DOC: 'https://sheets.googleapis.com/$discovery/rest?version=v4',
-    SCOPES: 'https://www.googleapis.com/auth/spreadsheets'
-};
